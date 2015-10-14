7497154bf3c464baaf0a920be15e1b621180c5d0
ardliath <ardliath@gmail.com>
2015-10-14 19:53:17 +0100

    Create script

---
+++ Scripts/Create.sql
@@ -0,0 +1,20 @@
+create table SolarSystems
+(
+	SolarSystemID int not null identity(1,1),
+	Name nvarchar(255),
+	X bigint not null,
+	Y bigint not null,
+	Z bigint not null,
+	TS timestamp not null
+	constraint PK_SolarSystems primary key (SolarSystemID)
+)
+
+create table Ships
+(
+	ShipID int not null identity(1,1),
+	Name nvarchar(255) not null,
+	SolarSystemID int null,
+	TS timestamp not null,
+	constraint PK_Ships primary key (ShipID),
+	constraint FK_Ships_SolarSystems foreign key (SolarSystemID) references SolarSystems(SolarSystemID)
+)
