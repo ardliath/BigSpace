66761b7214dcc72c2b3008d540603a08ab573113
ardliath <ardliath@gmail.com>
2015-10-18 10:53:21 +0100

    Updated scripts to include UserAccounts

--- Scripts/Create.sql
+++ Scripts/Create.sql
@@ -1,3 +1,22 @@
+
+CREATE TABLE [UserAccounts](
+	[UserAccountID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](255) NULL,
+	[X] [bigint] NOT NULL,
+	[Y] [bigint] NOT NULL,
+	[Z] [bigint] NOT NULL,
+	[TS] [timestamp] NOT NULL,
+ CONSTRAINT [PK_UserAccounts] PRIMARY KEY CLUSTERED 
+(
+	[UserAccountID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [UQ_Username] UNIQUE NONCLUSTERED 
+(
+	[Username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
 create table SolarSystems
 (
 	SolarSystemID bigint not null identity(1,1),
@@ -14,7 +33,9 @@ create table Ships
 	ShipID int not null identity(1,1),
 	Name nvarchar(255) not null,
 	SolarSystemID int null,
+	UserAccountID int not null,
 	TS timestamp not null,
 	constraint PK_Ships primary key (ShipID),
-	constraint FK_Ships_SolarSystems foreign key (SolarSystemID) references SolarSystems(SolarSystemID)
+	constraint FK_Ships_SolarSystems foreign key (SolarSystemID) references SolarSystems(SolarSystemID),
+	constraint FK_Ships_UserAccounts foreign key (UserAccountID) references UserAccounts(UserAccountID)
 )
