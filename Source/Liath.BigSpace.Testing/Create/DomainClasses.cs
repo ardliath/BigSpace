5934405c5367374fe3726004bb999ff22f4cd743
ardliath <ardliath@gmail.com>
2015-10-22 20:18:33 +0100

    CurrentUserNotFoundException_is_thrown_if_current_user_cannot_be_found test

---
+++ Source/Liath.BigSpace.Testing/Create/DomainClasses.cs
@@ -0,0 +1,18 @@
+﻿using Liath.BigSpace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.BigSpace.Testing.Create
+{
+    public class DomainClasses
+    {
+        public static ScreenSize ScreenSize(int? height = null, int? width = null)
+        {
+            return new ScreenSize(height.HasValue ? height.Value : 21,
+                width.HasValue ? width.Value : 21);
+        }
+    }
+}
--- Source/Liath.BigSpace.Tests/NavigationManagerTests/FindLocalSystemsTests.cs
+++ Source/Liath.BigSpace.Tests/NavigationManagerTests/FindLocalSystemsTests.cs
@@ -5,13 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using Liath.BigSpace.Definitions;
+using Liath.BigSpace.Domain;
+using Liath.BigSpace.Exceptions;
 
 namespace Liath.BigSpace.Tests.NavigationManagerTests
 {
     public class FindLocalSystemsTests
     {
         [Test]
-        public void Ensure_ArgumentNullException_is_thrown_if_screenSize_is_null()
+        public void ArgumentNullException_is_thrown_if_screenSize_is_null()
         {
             var manager = Create.BusinessLogicClass.NavigationManager();
 
@@ -19,6 +23,14 @@ namespace Liath.BigSpace.Tests.NavigationManagerTests
             Assert.AreEqual("screenSize", ex.ParamName);
         }
 
+        [Test]
+        public void CurrentUserNotFoundException_is_thrown_if_current_user_cannot_be_found()
+        {
+            var securityManager = new Mock<ISecurityManager>();
+            securityManager.Setup(x => x.GetCurrentUserAccount()).Returns<UserAccount>(null);
+            var manager = Create.BusinessLogicClass.NavigationManager(securityManager.Object);
 
+            Assert.Throws<CurrentUserNotFoundException>(() => manager.FindLocalSystems(Create.DomainClasses.ScreenSize()));
+        }
     }
 }
