ac5bbde9a1c87249b714f62a5faa70050e47cdfa
ardliath <ardliath@gmail.com>
2015-10-22 20:12:16 +0100

    First unit test
    Argument null exception and basic create class

---
+++ Source/Liath.BigSpace.Testing/Create/BusinessLogicClass.cs
@@ -0,0 +1,21 @@
+﻿using Liath.BigSpace.DataAccess.Definitions;
+using Liath.BigSpace.Definitions;
+using Liath.BigSpace.Implementations;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.BigSpace.Testing.Create
+{
+    public class BusinessLogicClass
+    {
+        public static NavigationManager NavigationManager(ISecurityManager securityManager = null, ISolarSystems solarSystems = null)
+        {
+            return new NavigationManager(securityManager ?? Mock.Of<ISecurityManager>(),
+                solarSystems ?? Mock.Of<ISolarSystems>());
+        }
+    }
+}
---
+++ Source/Liath.BigSpace.Tests/NavigationManagerTests/FindLocalSystemsTests.cs
@@ -0,0 +1,24 @@
+﻿using Create = Liath.BigSpace.Testing.Create;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liath.BigSpace.Tests.NavigationManagerTests
+{
+    public class FindLocalSystemsTests
+    {
+        [Test]
+        public void Ensure_ArgumentNullException_is_thrown_if_screenSize_is_null()
+        {
+            var manager = Create.BusinessLogicClass.NavigationManager();
+
+            var ex = Assert.Throws<ArgumentNullException>(() => manager.FindLocalSystems(null));
+            Assert.AreEqual("screenSize", ex.ParamName);
+        }
+
+
+    }
+}
