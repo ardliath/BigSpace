48872d5163d81ae3a9c0f3717a050b4205a34cb1
ardliath <ardliath@gmail.com>
2015-11-03 21:44:14 +0000

    Ajax calls to select and deselect a ship

---
+++ Source/Liath.BigSpace.UI.Web/Areas/OuterSpace/Controllers/ShipController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Liath.BigSpace.UI.Web.Areas.OuterSpace.Controllers
+{
+    public class ShipController : Controller
+    {
+        [HttpPost]
+        public ActionResult SelectShip(int id)
+        {
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        public ActionResult DeSelectShip(int id)
+        {
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
