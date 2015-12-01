abf88f25aeb93ac17e39d3319cd3973c12d2cb7b
ardliath <ardliath@gmail.com>
2015-12-01 22:05:08 +0000

    Created the controller, view and model

---
+++ Source/Liath.BigSpace.UI.Web/Areas/OuterSpace/Controllers/PlanetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Liath.BigSpace.UI.Web.Areas.OuterSpace.Models.Planet;
+
+namespace Liath.BigSpace.UI.Web.Areas.OuterSpace.Controllers
+{
+	public class PlanetController : Controller
+	{
+		[HttpGet]
+		public ActionResult Orbit(int id)
+		{
+			var model = new Orbit();
+			return View();
+		}
+	}
+}
---
+++ Source/Liath.BigSpace.UI.Web/Areas/OuterSpace/Models/Planet/Orbit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Liath.BigSpace.UI.Web.Areas.OuterSpace.Models.Planet
+{
+	public class Orbit
+	{
+	}
+}
