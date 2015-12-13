0caca5f81526dffecefd675978bcbff08061234b
ardliath <ardliath@gmail.com>
2015-12-13 12:17:29 +0000

    Fixed issue navigating around the screen

--- Source/Liath.BigSpace.UI.Web/Areas/OuterSpace/Controllers/LocalAreaController.cs
+++ Source/Liath.BigSpace.UI.Web/Areas/OuterSpace/Controllers/LocalAreaController.cs
@@ -14,67 +14,68 @@ namespace Liath.BigSpace.UI.Web.Areas.OuterSpace.Controllers
 {
 	public class LocalAreaController : Controller
 	{
-        private static ILogger logger = LogManager.GetCurrentClassLogger();
+		private static ILogger logger = LogManager.GetCurrentClassLogger();
 		private INavigationManager _navigationManager;
 		private ISessionManager _sessionManager;
-        private IUserManager _userManager;
+		private IUserManager _userManager;
 
-        public LocalAreaController(ISessionManager sessionManager, INavigationManager navigationManager, IUserManager userManager)
+		public LocalAreaController(ISessionManager sessionManager, INavigationManager navigationManager,
+			IUserManager userManager)
 		{
 			if (navigationManager == null) throw new ArgumentNullException("navigationManager");
 			if (sessionManager == null) throw new ArgumentNullException("sessionManager");
-            if (userManager == null) throw new ArgumentNullException("userManager");
+			if (userManager == null) throw new ArgumentNullException("userManager");
 
 			_navigationManager = navigationManager;
 			_sessionManager = sessionManager;
-            _userManager = userManager;
+			_userManager = userManager;
 		}
 
 		[HttpGet]
-        public ActionResult Display(Int64? id)
+		public ActionResult Display(Int64? id)
 		{
 			using (_sessionManager.CreateUnitOfWork())
 			{
-                if(id.HasValue)
-                {
-                    _navigationManager.FocusCurrentUserAccountOnSolarSystem(id.Value);
-                }
+				if (id.HasValue && id > 0)
+				{
+					_navigationManager.FocusCurrentUserAccountOnSolarSystem(id.Value);
+				}
 
 				var localSystems = _navigationManager.FindLocalSystems(new Liath.BigSpace.Domain.ScreenSize(21, 21));
-                return View(new Display
-                    {
-                        ScreenSize = new Models.LocalArea.ScreenSize
-                        {
-                            Height = 21,
-                            Width = 21
-                        },
-                        Systems = localSystems.SolarSystems.Select(s => new SystemSummary
-                        {
-                            ID = s.SolarSystemID,
-                            Name = s.Name,
-                            X = s.ScreenOffset.X,
-                            Y = s.ScreenOffset.Y
-                        })
-                    });
+				return View(new Display
+				{
+					ScreenSize = new Models.LocalArea.ScreenSize
+					{
+						Height = 21,
+						Width = 21
+					},
+					Systems = localSystems.SolarSystems.Select(s => new SystemSummary
+					{
+						ID = s.SolarSystemID,
+						Name = s.Name,
+						X = s.ScreenOffset.X,
+						Y = s.ScreenOffset.Y
+					})
+				});
 			}
 		}
 
-        [HttpPost]
-        public ActionResult UpdateFocusedCoordinates(UpdateView updateView)
-        {
-            try
-            {
-                using(_sessionManager.CreateUnitOfWork())
-                {
-                    _userManager.UpdateFocusedCoordinates(updateView.ChangeX, updateView.ChangeY, updateView.ChangeZ);
-                }
-                return new HttpStatusCodeResult(HttpStatusCode.OK);
-            }
-            catch(Exception ex)
-            {
-                logger.Error(ex, "There was an error updating the view");
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
-            }
-        }
+		[HttpPost]
+		public ActionResult UpdateFocusedCoordinates(UpdateView updateView)
+		{
+			try
+			{
+				using (_sessionManager.CreateUnitOfWork())
+				{
+					_userManager.UpdateFocusedCoordinates(updateView.ChangeX, updateView.ChangeY, updateView.ChangeZ);
+				}
+				return new HttpStatusCodeResult(HttpStatusCode.OK);
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex, "There was an error updating the view");
+				return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+			}
+		}
 	}
 }
