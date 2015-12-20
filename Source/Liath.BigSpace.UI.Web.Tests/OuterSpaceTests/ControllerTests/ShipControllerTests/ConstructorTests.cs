82dc4ca78e1fe979680491bc245a349fd1e8ec75
ardliath <ardliath@gmail.com>
2015-12-20 17:33:43 +0000

    Unit tests for Web project
    Some basic constructor tests but uses NUnit and FluentAssertions

---
+++ Source/Liath.BigSpace.UI.Web.Tests/OuterSpaceTests/ControllerTests/ShipControllerTests/ConstructorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Liath.BigSpace.Definitions;
+using Liath.BigSpace.Domain.DataAccessDefinitions;
+using Liath.BigSpace.Session;
+using Liath.BigSpace.UI.Web.Areas.OuterSpace.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Liath.BigSpace.UI.Web.Tests.OuterSpaceTests.ControllerTests.ShipControllerTests
+{
+	public class ConstructorTests
+	{
+		[Test]
+		public void Exception_is_thrown_if_sessionManager_is_null()
+		{
+			
+			Action act = () => new ShipController(null, Mock.Of<IFleetManager>(), Mock.Of<IEmpireManager>(), Mock.Of<IShips>());
+
+			act.ShouldThrow<ArgumentNullException>()
+				.And.ParamName.Equals("sessionManager");
+		}
+
+
+		[Test]
+		public void Exception_is_thrown_if_fleetManager_is_null()
+		{
+
+			Action act = () => new ShipController(Mock.Of<ISessionManager>(), null, Mock.Of<IEmpireManager>(), Mock.Of<IShips>());
+
+			act.ShouldThrow<ArgumentNullException>()
+				.And.ParamName.Equals("fleetManager");
+		}
+
+
+		[Test]
+		public void Exception_is_thrown_if_empireManager_is_null()
+		{
+
+			Action act = () => new ShipController(Mock.Of<ISessionManager>(), Mock.Of<IFleetManager>(), null, Mock.Of<IShips>());
+
+			act.ShouldThrow<ArgumentNullException>()
+				.And.ParamName.Equals("empireManager");
+		}
+
+
+		[Test]
+		public void Exception_is_thrown_if_ships_is_null()
+		{
+
+			Action act = () => new ShipController(Mock.Of<ISessionManager>(), Mock.Of<IFleetManager>(), Mock.Of<IEmpireManager>(), null);
+
+			act.ShouldThrow<ArgumentNullException>()
+				.And.ParamName.Equals("ships");
+		}
+
+
+		[Test]
+		public void No_exception_is_thrown_if_all_arguments_are_non_null()
+		{
+
+			Action act = () => new ShipController(Mock.Of<ISessionManager>(), Mock.Of<IFleetManager>(), Mock.Of<IEmpireManager>(), Mock.Of<IShips>());
+
+			act.ShouldNotThrow<Exception>();
+		}
+	}
+}
---
+++ Source/Liath.BigSpace.UI.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Liath.BigSpace.UI.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Liath.BigSpace.UI.Web.Tests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42a8b672-af72-4251-9e41-40fafc851d98")]
